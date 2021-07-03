f6189ed1c352bf6e246c1d31c2ecfcc9e6fd3cfd
LucasGuzman38 <lucasguzmangg@gmail.com>
2021-07-03 13:20:49 -0300

    Tercer Commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola anashex32322323");
+	alert("Hola bosta andante");
 }
 
