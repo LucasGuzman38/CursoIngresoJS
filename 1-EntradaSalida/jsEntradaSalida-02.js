33e46ac64395f2a8649da0cfc222f76d1bc69d9b
LucasGuzman38 <lucasguzmangg@gmail.com>
2021-07-06 11:58:29 -0300

    Primera clase

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,12 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
+	// declaracion de una variable palabra reservada let nombreVariable;
+		let dato; // declaro la variable
 
+		dato = prompt("Ingrese su nombre");
+		
+		alert(dato);
+
+
+}
