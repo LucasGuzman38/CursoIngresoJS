c6b39a1d91492564a3e470bb3e08f6d7b7639408
LucasGuzman38 <lucasguzmangg@gmail.com>
2021-07-11 15:48:30 -0300

    Cuarto Commit

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,54 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let numero1;
+	let numero2;
+	let suma;
+	
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	suma = numero1 + numero2;
+	alert("El resultado es " + suma);
 }
 
 function restar()
 {
+	let numero1;
+	let numero2;
+	let resta;
 	
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resta = numero1 - numero2;
+	alert("El resultado es " + resta);
 }
 
 function multiplicar()
-{ 
+{
+	let numero1;
+	let numero2;
+	let multiplicacion;
+	
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	multiplicacion = numero1 * numero2;
+	alert("El resultado es " + multiplicacion);
 	
 }
 
 function dividir()
 {
+	let numero1;
+	let numero2;
+	let division;
 	
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	division = numero1 / numero2;
+	alert("El resultado es " + division);
 }
 
